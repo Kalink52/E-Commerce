012e83826b311d066bdeb0fd76485d6ef4eb6733
Kalink52 <micah.cox52@gmail.com>
2024-07-12 21:12:18 -0400

    models done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,32 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData)
+
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findByPk(req.params.id);
+    if (!categoryData){
+      res.status(400).json({message: 'Category key could not be found!'})
+      return
+    }
+    res.status(200).json(categoryData)
+
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const productData = await Product.findAll()
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id);
+    if (!productData){
+      res.status(404).json({message: 'product could not be found'})
+      return
+    }
+    res.status(202).json(productData)
+
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // create new product
